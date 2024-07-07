ce131b2c9badb38f322c93602d2b5707a034196a
Ilham <ilhamushul@gmail.com>
2024-07-07 17:21:05 +0700

    change: slideToScroll to 1 at breakpoint 480px

--- src/components/HotMovies.jsx
+++ src/components/HotMovies.jsx
@@ -53,7 +53,7 @@ const HotMovies = () => {
         breakpoint: 480,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
         },
       },
     ],
